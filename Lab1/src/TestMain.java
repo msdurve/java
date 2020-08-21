245b7990ce1302a6693cc5f3e647d8f14fc4ba53
Mayank <Mayank>
2020-08-21 23:01:33 +0530

    Challenge 1

--- Lab1/src/TestMain.java
+++ Lab1/src/TestMain.java
@@ -3,5 +3,6 @@ public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("Hello MSD");
 		System.out.println("Git Hello");
+		System.out.println("Hello Mayank") ;  // Challenge1 
 	}
 }
