3e6b16a315e5ac7273ba22bef0c346a577e9276d
Mayank <Mayank>
2020-08-24 10:41:41 +0530

    Challenge2 --Variables

--- Lab1/src/TestMain.java
+++ Lab1/src/TestMain.java
@@ -4,5 +4,9 @@ public class TestMain {
 		System.out.println("Hello MSD");
 		System.out.println("Git Hello");
 		System.out.println("Hello Mayank") ;  // Challenge1 
+		
+		int myFirstNumber = 5 ; 
+		System.out.println("My First Number is : " + myFirstNumber) ;
+		
 	}
 }
